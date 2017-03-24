737bbacaaeea2a7ff039ec471ca1c41bcc74d103
Daniel Thompson <19dst89@gmail.com>
2017-03-24 11:42:07 -0700

    render product index, fix product controller issues

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,53 +1,48 @@
 class ProductsController < ApplicationController
 
-      def index
-        @products=Product.all
-      end
-      def new
-        @product = Product.new
-      end
-      def show
-        product_id = params[:id]
-        @product = Product.find_by_id(product_id)
-      end
-      def create
-        product_params = params.require(:product).permit(:name, :description, :min_price, :total_donated_amount, :inventory_amount, :avatar)
-        product = Product.new(product-params)
-          if Product.save
-            redirect_to product_path
-          end
-      end
-      def show
-        product_id = params[:id]
-        @product = Product.find_by_id(product_id)
-      end
-      def edit
-        product_id = params[:id]
-        @product = Product.find_by_id(product_id)
-      end
-      def update
+def index
+  @products = Product.all
+end
 
-        product_id = params[:id]
-        product = Product.find_by_id(product_id)
-        product.update_attributes(product_params)
-        redirect_to product_path(product)
+def new
+  @product = Product.new
+end
 
-      end
+def show
+  product_id = params[:id]
+  @product = Product.find_by_id(product_id)
+end
 
+def create
+  @product = Product.new(product_params)
+    if Product.save
+      redirect_to product_path
+    end
+end
 
-      def destroy
-        product_id = params[:id]
-        product = Product.find_by_id(product_id)
-        product.destroy
-        redirect_to products_path
-      end
+def edit
+  product_id = params[:id]
+  @product = Product.find_by_id(product_id)
+end
 
-      private
+def update
+  product_id = params[:id]
+  @product = Product.find_by_id(product_id)
+  @product.update_attributes(product_params)
+  redirect_to product_path(@product)
+end
+
+def destroy
+  product_id = params[:id]
+  @product = Product.find_by_id(product_id)
+  @product.destroy
+  redirect_to products_path
+end
+
+  private
 
   def product_params
-
     params.require(:product).permit(:name, :description, :min_price, :total_donated_amount, :inventory_amount, :avatar)
   end
-
 
 end
