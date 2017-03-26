97a2434eba4016838e5009c23b3a019d63aa4053
Daniel Thompson <19dst89@gmail.com>
2017-03-26 00:38:28 -0700

    some basic views for transactions but the associations for everything are not working quite right for some reason

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,35 @@
 class TransactionsController < ApplicationController
+
+  def index
+    @products = Product.all
+    @users = User.all
+  end
+
+  def show
+    transaction_id = params[:id]
+    @transaction = Transaction.find(transaction_id)
+  end
+
+  def new
+    @transaction = Transaction.new
+    # currently hardcoding product until i figure out how to grab it dynamically
+    @product = Product.first
+  end
+
+  def create
+    @transaction = Transaction.new(transaction_params)
+    @transaction.users_id = current_user.id
+    # currently hardcoding product id until i figure out how to grab it dynamically
+    @transaction.products_id = Product.first.id
+    if @transaction.save
+      redirect_to @transaction
+    end
+  end
+
+
+  private
+
+  def transaction_params
+    params.require(:transaction).permit(:user_id, :product_id)
+  end
 end
