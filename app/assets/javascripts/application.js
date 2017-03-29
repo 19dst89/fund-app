8a8212b90049728de5a473785bd9255e0c473666
zeroCalm <smith.timothyh@gmail.com>
2017-03-29 10:23:03 -0700

    Clock is again showing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require_tree .
 //= require jquery
 //= require jquery.turbolinks
-//= require jquery_ujs
 //= require countdown
+//= require jquery_ujs
 //= require turbolinks
- 
--- app/assets/javascripts/countdown.js
+++ app/assets/javascripts/countdown.js
@@ -1,34 +1,35 @@
-// Credit goes to StackOverflow communit for this JS file.
+// Credit goes to StackOverflow community for this JS file.
 //
 // http://stackoverflow.com/questions/9335140/how-to-countdown-to-a-date#9335296
 //
- 
+
 var end = new Date('03/30/2017 12:00 AM');
 
-    var _second = 1000;
-    var _minute = _second * 60;
-    var _hour = _minute * 60;
-    var _day = _hour * 24;
-    var timer;
+var _second = 1000;
+var _minute = _second * 60;
+var _hour = _minute * 60;
+var _day = _hour * 24;
+var timer;
 
-    function showRemaining() {
-        var now = new Date();
-        var distance = end - now;
-        if (distance < 0) {
+function showRemaining() {
+    console.log("show remaining called");
+    var now = new Date();
+    var distance = end - now;
+    if (distance < 0) {
 
-            clearInterval(timer);
-            document.getElementById('countdown').innerHTML = 'EXPIRED!';
+        clearInterval(timer);
+        document.getElementById('countdown').innerHTML = 'EXPIRED!';
 
-            return;
-        }
-        var days = Math.floor(distance / _day);
-        var hours = Math.floor((distance % _day) / _hour);
-        var minutes = Math.floor((distance % _hour) / _minute);
-        var seconds = Math.floor((distance % _minute) / _second);
-
-        document.getElementById('countdown').innerHTML = days + 'days ';
-        document.getElementById('countdown').innerHTML += hours + 'hrs ';
-        document.getElementById('countdown').innerHTML += minutes + 'mins ';
-        document.getElementById('countdown').innerHTML += seconds + 'secs';
+        return;
     }
-     timer = setInterval(showRemaining, 1000);
+    var days = Math.floor(distance / _day);
+    var hours = Math.floor((distance % _day) / _hour);
+    var minutes = Math.floor((distance % _hour) / _minute);
+    var seconds = Math.floor((distance % _minute) / _second);
+
+    document.getElementById('countdown').innerHTML = days + 'days ';
+    document.getElementById('countdown').innerHTML += hours + 'hrs ';
+    document.getElementById('countdown').innerHTML += minutes + 'mins ';
+    document.getElementById('countdown').innerHTML += seconds + 'secs';
+}
+timer = setInterval(showRemaining, 1000);
