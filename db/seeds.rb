76a58a2edb2def33d04b4ba549a8731789ba3e0d
cnwilliams <hello.christenwilliams@gmail.com>
2017-03-31 00:14:55 -0700

    adds auth to users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+
+Product.create({
+  name: "Product Name Here",
+  description: "Add your description here... Say cheese fondue fromage frais. Halloumi cheese slices bavarian bergkase danish fontina cheese strings smelly cheese squirty cheese cauliflower cheese. Red leicester taleggio red leicester croque monsieur babybel chalk and cheese blue castello who moved my cheese. The big cheese cheesy grin danish fontina jarlsberg emmental who moved my cheese pecorino roquefort. Brie caerphilly rubber cheese babybel hard cheese say cheese cauliflower cheese cheese triangles.",
+  min_price: 20,
+  total_donated_amount: 0,
+  inventory_amount: 10,
+  avatar: ""
+  })
+
+
+
